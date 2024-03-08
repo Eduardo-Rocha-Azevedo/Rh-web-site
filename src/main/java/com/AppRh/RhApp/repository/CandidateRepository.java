d5a33a9f0310affc6b07a549d74fcf80bcb52782
Eduardo-Rocha-Azevedo <eduazevedo5414@gmail.com>
2024-03-08 12:02:48 -0300

    bug fix

--- src/main/java/com/AppRh/RhApp/repository/CandidateRepository.java
+++ src/main/java/com/AppRh/RhApp/repository/CandidateRepository.java
@@ -3,8 +3,11 @@ package com.AppRh.RhApp.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.AppRh.RhApp.models.Candidate;
 import com.AppRh.RhApp.models.Vaga;
+@Repository
 public interface CandidateRepository extends CrudRepository<Candidate, String>{
 
     Iterable<Candidate> findByVaga(Vaga vaga);
--- src/main/java/com/AppRh/RhApp/repository/VagaRepository.java
+++ src/main/java/com/AppRh/RhApp/repository/VagaRepository.java
@@ -3,8 +3,11 @@ package com.AppRh.RhApp.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.AppRh.RhApp.models.Vaga;
 
+@Repository
 public interface VagaRepository extends CrudRepository<Vaga, String>{
     Vaga findByCodigo(long codigo);
     List<Vaga> findByNome(String nome);
